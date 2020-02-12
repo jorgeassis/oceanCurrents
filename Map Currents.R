5f112d008d9d6e493821d0dd5d97e93d34babd9a
Jorge Assis <jorgeassis@jorges-mac-pro.ualg.pt>
2020-02-12 11:34:30 +0000

    Up.

--- Map Currents.R
+++ Map Currents.R
@@ -10,17 +10,17 @@
 ## ---------------------------------------------------------------------------------------------------------
 
 source("mainFunctions.R")
+library(rnaturalearth)
+library(rgeos)
 
 number.cores <- 2
 
-directory <- "/Volumes/Jellyfish/Dropbox/Manuscripts/Bio-ORACLE Across Climate Changes/Bioclimatic Layers/Present/Surface_HR/LongTerm"
+directory <- "/Volumes/Jellyfish/Dropbox/Manuscripts/Bio-ORACLE Across Climate Changes/Bioclimatic Layers/Present/Surface/LongTerm"
 # directory <- "/Volumes/Jellyfish/Dropbox/Manuscripts/Bio-ORACLE Across Climate Changes/Bioclimatic Layers/Past LGM/Surface/LongTerm"
 
 ## --------------------------------------
 
-xmin <- -175 ; xmax <- -104.75 ; ymin <- 20  ; ymax <- 62.5
-xmin <- -180 ; xmax <- 180 ; ymin <- -70  ; ymax <- 0
-xmin <- -6.5 ; xmax <- 2.6 ; ymin <- 46.32  ; ymax <- 51.9
+xmin <- -30 ; xmax <- 20 ; ymin <- -20  ; ymax <- 30
 resolution <- 0.01
 
 region.as.table <- matrix( NA ,nrow= ((ymax-ymin)/resolution) ,ncol= ((xmax-xmin)/resolution) )
@@ -28,14 +28,23 @@ region.as.raster <- raster(region.as.table)
 extent(region.as.raster) <- c(xmin,xmax,ymin,ymax)
 crs(region.as.raster) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
+world <- ne_countries(scale = 'large')
+world <- crop(world,region.as.raster)
+world <- gUnaryUnion(world, id = rep(1,length(world)))
+region.as.raster <- rasterize(world,region.as.raster)
+region.as.raster[is.na(region.as.raster)] <- -9
+region.as.raster[region.as.raster != -9] <- NA
+c[region.as.raster == -9] <- 1
+plot(region.as.raster)
+
 ## -----------------------------------------------------------------------------------
 ## -----------------------------------------------------------------------------------
 
-Meridional.Current <- list.files(directory,pattern="Sea.water.X.velocity",full.names = TRUE)
+Meridional.Current <- list.files(directory,pattern="SeaWaterVelocityX",full.names = TRUE)
 Meridional.Current <- Meridional.Current[which(!grepl("Range", Meridional.Current))]
 Meridional.Current <- Meridional.Current[which(!grepl("Summary", Meridional.Current))]
 
-Zonal.Current <- list.files(directory,pattern="Sea.water.Y.velocity",full.names = TRUE)
+Zonal.Current <- list.files(directory,pattern="SeaWaterVelocityY",full.names = TRUE)
 Zonal.Current <- Zonal.Current[which(!grepl("Range", Zonal.Current))]
 Zonal.Current <- Zonal.Current[which(!grepl("Summary", Zonal.Current))]
 
@@ -73,7 +82,13 @@ w2hd <- brick(w2hd.1, w2hd.2)
 slope <- sqrt((w2hd[[1]]*10)^2 + (w2hd[[2]]*10)^2)
 aspect <- atan2(w2hd[[1]]*10, w2hd[[2]]*10)
 vectorplot(w2hd * 10, isField = "dXY", region = slope/10, margin = FALSE, par.settings = rasterTheme(region = matlab.like(n = 100)), 
-           narrows = 5000, at = seq(min(getValues(slope),na.rm=T)/10,max(getValues(slope),na.rm=T)/10,length.out=100), col.arrows='white')
+           narrows = 1000, at = seq(min(getValues(slope),na.rm=T)/10,max(getValues(slope),na.rm=T)/10,length.out=100), col.arrows='white')
+
+slope <- sqrt(w2hd[[1]]^2 + w2hd[[2]]^2)
+aspect <- atan2(w2hd[[1]], w2hd[[2]])
+vectorplot(w2hd * 75, isField = "dXY", lwd.arrows=0.7, region = slope, margin = FALSE, par.settings = rasterTheme(region = matlab.like(n = 120)[-c(1:20)]), narrows = 1000, at = seq(min(getValues(slope),na.rm=T),max(getValues(slope),na.rm=T),length.out=100)) + latticeExtra::layer(sp.polygons(world, col = "#E9E9E9",fill="#E9E9E9"))
+
+vectorplot(w2hd * 75, isField = "dXY", lwd.arrows=0, region = slope, margin = FALSE, par.settings = rasterTheme(region = matlab.like(n = 120)[-c(1:20)]), narrows = 1000, at = seq(min(getValues(slope),na.rm=T),max(getValues(slope),na.rm=T),length.out=100)) + latticeExtra::layer(sp.polygons(world, col = "#E9E9E9",fill="#E9E9E9")) +  vectorplot(w2hd * 75, isField = "dXY", region = FALSE, margin = FALSE)
 
 ## -----------------------------------------------------------------------------------
 ## -----------------------------------------------------------------------------------
